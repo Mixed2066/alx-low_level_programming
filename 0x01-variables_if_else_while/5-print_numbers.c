a823e0f9a2a6b73a19ba084fb37f7ff14449a654
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-02 11:57:49 +0000

    Prints numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Return 0 (execution success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+return (0); }
