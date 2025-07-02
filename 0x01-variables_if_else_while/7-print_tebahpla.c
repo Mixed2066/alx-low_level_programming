b6f040e13e00b27e92b5ff0dbf9600709130b4d5
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-02 11:59:19 +0000

    Prints alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Return 0 (execution success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0); }
