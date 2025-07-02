3ca5785ba948cb651c64ca335c7b454540e1d0d7
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-02 11:55:29 +0000

    Prints lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Return 0 (execution success)
+ */
+int main(void)
+{
+    /*Declaring ch*/
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+    /*prints abcdefg... */
+putchar(ch);
+}
+return (0); }
