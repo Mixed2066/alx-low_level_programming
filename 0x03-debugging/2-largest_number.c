42af2a96dad320461f9d4ecc0d37400e61d0f611
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-15 10:12:35 +0000

    Fixed indention

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-/* a - 972, b - -98, c - 0 */
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+int largest;
 
-    return (largest);
+if (a > b && a > c)
+{
+largest = a;
 }
+else if (b > a && b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest); }
