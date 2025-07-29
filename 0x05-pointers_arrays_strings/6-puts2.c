df06d187044b173afb6b1c450f572212c2b6233c
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-29 08:44:20 +0000

    Prints half of the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - Prints every second character of a string
+ * @str: The input string
+ *
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; i <= str[i]; i++)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+else
+continue;
+}
+_putchar('\n');
+}
