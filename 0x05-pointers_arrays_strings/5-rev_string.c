642362557d25fb17b51491d3f26c047a95d5284f
Mixed2066 <mixedtutorials299@gmail.com>
2025-07-29 08:43:54 +0000

    Prints the string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - Reverses the string
+ * @s: The input string
+ *
+ */
+void rev_string(char *s)
+{
+int length = 0, i;
+for (length = 0; s[length] != '\0'; length++)
+{
+
+}
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
