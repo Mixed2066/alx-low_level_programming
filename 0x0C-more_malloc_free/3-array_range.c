81682e4383755d8ebfcfc683f044cf1199a64053
Mixed2066 <mixedtutorials299@gmail.com>
2025-11-06 11:14:00 +0000

    used astyle

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,15 +2,15 @@
 #include "main.h"
 int *array_range(int min, int max)
 {
-int *arr;
-int i, size;
-if (min > max)
-return (NULL);
-size = max - min + 1;
-arr = malloc(sizeof(int) * size);
-if (arr == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-arr[i] = min + i;
-return (arr);
+    int *arr;
+    int i, size;
+    if (min > max)
+        return (NULL);
+    size = max - min + 1;
+    arr = malloc(sizeof(int) * size);
+    if (arr == NULL)
+        return (NULL);
+    for (i = 0; i < size; i++)
+        arr[i] = min + i;
+    return (arr);
 }
