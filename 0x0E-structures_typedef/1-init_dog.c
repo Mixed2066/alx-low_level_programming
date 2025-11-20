702107e062644e28276d7cc70941f358cf901bd2
Mixed2066 <mixedtutorials299@gmail.com>
2025-11-20 10:13:40 +0000

    initializes a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+else
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
